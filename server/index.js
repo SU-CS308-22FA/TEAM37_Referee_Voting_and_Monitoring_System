8f2d034d9bc0e5b371cbcd59a71bd43d7e240d03
ydelipinar <ydelipinar.sabanciuniv.edu>
2022-10-30 16:57:45 +0300

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://ydelipinar:ydelipinar123123@cluster0.mqafbuk.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
