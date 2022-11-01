7cc5916687612b00c44cea248cb0e18477935231
ydelipinar <ydelipinar.sabanciuniv.edu>
2022-11-01 19:07:31 +0300

    backend for sign in sing up created

---
+++ client/src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
+
+  return req;
+});
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
--- server/index.js
+++ server/index.js
@@ -5,9 +5,14 @@ import cors from 'cors';
 
 const app = express();
 
+import userRouter from "./routes/user.js";
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://ydelipinar:ydelipinar123123@cluster0.mqafbuk.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
