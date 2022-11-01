8796339d3aee73361fa1c73cb087aa0f3832c893
mertkilicaslan <mkilicaslan@sabanciuniv.edu>
2022-11-01 17:22:24 +0300

    code improvements

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://ydelipinar:ydelipinar123123@cluster0.mqafbuk.mongodb.net/?retryWrites=true&w=majority';
@@ -14,5 +14,5 @@ const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error.message));
 
