642b77acfab2b216978ee6cda7cdd948414c4919
mertkilicaslan <mkilicaslan@sabanciuniv.edu>
2022-11-01 18:57:50 +0300

    front end improvements

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
-import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-function App() {
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>Hello</div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.AppBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Refree System
+        </Typography>
+      </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
-}
+};
 
 export default App;
