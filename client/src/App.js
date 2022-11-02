df93d74758d258c0cede736d863b17b93a4ed56c
mertkilicaslan <mkilicaslan@sabanciuniv.edu>
2022-11-02 14:10:33 +0300

    front-end modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Register from "./components/Register";
 
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
-
-const App = () => {
-  const classes = useStyles();
-
+function App() {
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.AppBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Referee Voting and Monitoring System
-        </Typography>
-      </AppBar>
-
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <main className="App">
+      <Register />
+    </main>
   );
-};
+}
 
 export default App;
