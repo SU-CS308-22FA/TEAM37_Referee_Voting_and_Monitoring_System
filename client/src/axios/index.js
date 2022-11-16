74c598c60e3c1f98c792a4d79637b28da368d00b
ydelipinar <ydelipinar.sabanciuniv.edu>
2022-11-16 19:29:59 +0300

    test

---
+++ client/src/axios/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+
+
+const HTTP = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+export const login = async (formData) =>
+  await HTTP.post("/users/signin", formData);
+
+export const register = async (formData) =>
+  await HTTP.post("/users/signup", formData);
+
+export const handleEdit = async (formdata, id) => {    
+    try {
+      const response = await HTTP.put(`/users/${id}`, formdata);
+      
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+}
+export const handledelete = async (formdata, id) => {    
+  try {
+    const response1 = await HTTP.delete(`/users/${id}`, formdata);
+    
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
+}
+export const handleSignin = async (data) => {
+  try {
+    const { data: res } = await HTTP.post("/users/signin", data);
+    console.log(res);
+    sessionStorage.setItem("token", res.data);
+    sessionStorage.setItem("user", JSON.stringify(res.user));
+    window.location = "/";
+
+  } catch (err) {
+
+      console.log(`Error: ${err.message}`);
+      return err;
+   };
+}
---
+++ server/controllers/userController.js
@@ -0,0 +1,91 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/userModel.js";
+
+const secret = 'test';
+
+export const signin = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+      if (!password || !email)
+        {
+          return res.status(400).json({ message: "All fields required" });
+        }
+      const user = await User.findOne({ email });
+      if (!user) return res.status(400).json({ message: "User does not exist." });
+  
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect)
+        return res.status(400).json({ message: "Wrong password" });
+
+        const token = jwt.sign({ email: user.email, id: user._id }, secret, { expiresIn: "1h" });
+
+      return res.status(200).json({ result: user, token });
+       
+    } 
+    catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  };
+
+  export const signup = async (req, res) => {
+    const { fullname, password, phoneNumber, email } = req.body;
+  
+    try {
+        
+        if (!fullname || !password || !email)
+        {
+          return res.status(400).json({ message: "All fields required" });
+        }
+        const userExists = await User.findOne({ email });
+  
+      if (userExists) return res.status(400).json({ message: "User already exists" });
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const createdUser = await User.create({
+        fullname,
+        email,
+        password: hashedPassword,
+        phoneNumber,
+      });
+  
+      const token = jwt.sign( { email: createdUser.email, id: createdUser._id }, secret, { expiresIn: "1h" } );
+  
+      res.status(201).json({ createdUser, token });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+      
+      console.log(error);
+    }
+  };
+
+ export const updateUser = async (req, res) => {
+    
+    const { fullname } = req.body;
+    try {
+          const updatedUser = await User.findByIdAndUpdate(
+              req.params.id,
+              { $set: {fullname} },
+              { new: true }
+            );
+          
+          return res.status(200).json({ updatedUser, message: 'Informations changed succesfully' })
+      } catch (error) {
+          return res.status(400).json({ message: error.message})
+      }
+  };
+
+  export const deleteUser = async (req, res) => {
+    try {
+         await User.findByIdAndDelete(req.params.id);
+         res.status(200).json("User has been deleted.");
+      } 
+      catch (err) {
+        return res.status(400).json({ message: err.message })
+      }
+};
