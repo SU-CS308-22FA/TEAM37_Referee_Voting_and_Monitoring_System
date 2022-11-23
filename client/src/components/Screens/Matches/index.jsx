10f82791b5bd6d9a15c871d4b763e16902b0937e
ydelipinar <ydelipinar@sabanciuniv.edu>
2022-11-23 22:20:59 +0300

    standings minor bug fix

--- client/src/components/Screens/Matches/index.jsx
+++ client/src/components/Screens/Matches/index.jsx
@@ -10,37 +10,11 @@ import './table.css';
 
 const Matches = () => {
   
-
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  const [team, setteam] = useState("test");
-  
-   
-  // useEffect(() => {
-  //   // 👇️ this only runs once
-  //   console.log('useEffect ran');
+  const [team, setteam] = useState();
+ 
 
-  //   // 👇️ fetch data from remote API
-
-  //   async function getApi() {
-  //     try {
-  //       const response = await axios(config);
-  //          const standings = response.data.response.league.standings[0]
-  //       setteam(standings);
-  // //     localStorage.setItem("teams", JSON.stringify(standings);
-  // //     console.log(response.data);
-
-       
-
-  //       console.log(team);
-
-  //     } catch (err) {
-  //       console.log(error);
-  //     }
-  //   }
-
-  //   getApi();
-  // }, []);
   var config = {
             method: 'get',
             url: 'https://v3.football.api-sports.io/standings',
@@ -55,9 +29,15 @@ const Matches = () => {
   useEffect(() => {
 		if (window.localStorage !== undefined) {
 			const data = window.localStorage.getItem('teams');
+      console.log("use effect called");
+
+      console.log(team);
       try{
         if(data !== null){
           setteam(JSON.parse(data))
+          console.log("INSIDE IF");
+          console.log(team);
+
         } 
       }
      catch (err) {
@@ -82,54 +62,55 @@ const Matches = () => {
 	};
   const print = async () => {
 		const data = window.localStorage.getItem('teams');
+    console.log("print called");
     console.log(team);
+  
 	};
 
  
   return (
     <>
-
-        
-        <button onClick={callAPI}>Call API</button>
-        <button onClick={print}>Print</button>
+     
+        <button onClick={callAPI} className={styles.green_btn} align = "center">
+         Call API
+        </button>
        
         <div className="App">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 text-center mt-2">
-                       
-                        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center mt-2">
+                    
                     </div>
-                
-                    <div className="table-responsive mt-5">
-                        <table className="table">
-                        <thead><tr><td colSpan="9"><h3>{"TFF"}</h3></td></tr><tr><th className="position">#</th><th className="team" colSpan="2">Team</th><th className="played">Played</th><th className="won">Won</th><th className="draw">Draw</th><th className="lost">Lost</th><th className="ga">GF</th><th className="ga">GA</th><th className="points">Points</th></tr></thead>;
-                          <tbody>
-                            {team.map(standing => (
-                              
-                            <tr >
-                              <td>{standing.rank}</td>
-                              
-                              <td className="badge-td"><div className="badge"><img src={standing.team.logo} alt={standing.team.name} /></div></td>
-                              <td className="text-left">{standing.team.name}</td>
-                              <td>{standing.all.played}</td>
-                              <td>{standing.all.win}</td>
-                              <td>{standing.all.draw}</td>
-                              <td>{standing.all.lose}</td>
-                              <td>{standing.all.goals.for}</td>
-                              <td>{standing.all.goals.against}</td>
-                              
-                              <td>{standing.points}</td>
-
-                           </tr>
-                            ))}
-                         </tbody>
-                        </table>
-                    </div>
-
+                </div>           
+                  <table className="table" size={window.outerWidth } align = "center">
+                    <thead>
+                      <tr>
+                         <td colSpan="9"><h3>{"TFF"}</h3></td>
+                      </tr>
+                      <tr>
+                         <th className="position">#</th><th className="team" colSpan="2">Team</th><th className="played">Played</th><th className="won">Won</th><th className="draw">Draw</th><th className="lost">Lost</th><th className="ga">GF</th><th className="ga">GA</th><th className="points">Points</th>
+                      </tr>
+                    </thead>;
+                      <tbody>
+                      {team && team.map((standing, key) =>{
+                        
+                        return <tr key={key}>
+                        <td>{standing.rank}</td>                                                  
+                        <td className="badge-td"><div className="badge"><img src={standing.team.logo} alt={standing.team.name} /></div></td>
+                        <td className="text-left">{standing.team.name}</td>
+                        <td>{standing.all.played}</td>
+                        <td>{standing.all.win}</td>
+                        <td>{standing.all.draw}</td>
+                        <td>{standing.all.lose}</td>
+                        <td>{standing.all.goals.for}</td>
+                        <td>{standing.all.goals.against}</td>                          
+                        <td>{standing.points}</td>
+                        </tr>
+                      })}
+                      </tbody>
+                    </table>
                 </div>
-            </div>
- 
+            </div>       
 </>
   )
 }
