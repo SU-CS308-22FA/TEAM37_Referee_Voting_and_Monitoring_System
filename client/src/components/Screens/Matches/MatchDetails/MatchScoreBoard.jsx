328264ea0d89b762b95aae3eafdeea3be13980c5
mertkilicaslan <mkilicaslan@sabanciuniv.edu>
2022-12-30 02:32:10 +0300

    Improvements on the match details section Match detail section has been improved with additional display information such as the referee name, stadium location, time zone and the season the match took place

--- client/src/components/Screens/Matches/MatchDetails/MatchScoreBoard.jsx
+++ client/src/components/Screens/Matches/MatchDetails/MatchScoreBoard.jsx
@@ -5,33 +5,32 @@ import { GiWhistle } from "react-icons/gi";
 import { TbClock } from "react-icons/tb";
 import { MdArrowBackIosNew, MdOutlinePlace } from "react-icons/md";
 
-const MatchGeneralInfo = ({ data }) => {
+const MatchScoreBoard = ({ data }) => {
   console.log(data);
 
   return (
-    <div className="container">
+    <div className="match-page-general-info-container">
       <div className="grid-container">
         <Link to="/matches" className="back-to-mathces-button">
           <MdArrowBackIosNew />
           <span>Matches</span>
         </Link>
         <div className="middle-grid-item">
-          <a className="header-league-css">
+          <Link className="header-league-css" to="/standing">
             <div className="league-icon league-icon-container">
               <img
                 alt="League Logo"
-                width="30"
-                height="30"
+                width="25"
+                height="25"
                 src={data.league.logo}
-                className="Image"
               />
             </div>
-            <Link to="/standing">
-              <span>
-                {data.league.name} - {data.league.round}
-              </span>
-            </Link>
-          </a>
+
+            <div>
+              {data.league.name}&nbsp; | &nbsp;{data.league.round}
+              &nbsp; | &nbsp;{data.league.season}
+            </div>
+          </Link>
         </div>
       </div>
 
@@ -90,7 +89,7 @@ const MatchGeneralInfo = ({ data }) => {
                     {aMatchEvent.player.name + " "}
                   </span>
                   <span className="event-time">
-                    {aMatchEvent.time.elapsed}
+                    ({aMatchEvent.time.elapsed})
                     {aMatchEvent.detail === "Penalty" ? " (P)" : ""}
                   </span>
                 </li>
@@ -118,7 +117,7 @@ const MatchGeneralInfo = ({ data }) => {
                     {aMatchEvent.player.name + " "}
                   </span>
                   <span className="event-time">
-                    {aMatchEvent.time.elapsed}
+                    ({aMatchEvent.time.elapsed})
                     {aMatchEvent.detail === "Penalty" ? " (P)" : ""}
                   </span>
                 </li>
@@ -129,21 +128,33 @@ const MatchGeneralInfo = ({ data }) => {
       <div className="match-details-info-container">
         <ul className="match-detail-box">
           <li className="match-detail-item">
-            <TbClock />
             <time dateTime={data.fixture.date}>
-              <span>TIME</span>,
+              <p>
+                <TbClock />
+                Time
+              </p>
+              <span>
+                {data.fixture.date.slice(0, 10) + " "}
+                {new Date(data.fixture.date).toTimeString().slice(0, 5)}
+              </span>
             </time>
           </li>
 
           <li className="match-detail-item">
-            <MdOutlinePlace />
+            <p>
+              <MdOutlinePlace />
+              Stadium
+            </p>
             <span>
               {data.fixture.venue.name}, {data.fixture.venue.city}
             </span>
           </li>
 
           <li className="match-detail-item">
-            <GiWhistle />
+            <p>
+              <GiWhistle />
+              Referee
+            </p>
             <span>
               {data.fixture.referee.slice(0, data.fixture.referee.indexOf(","))}
             </span>
@@ -154,4 +165,4 @@ const MatchGeneralInfo = ({ data }) => {
   );
 };
 
-export default MatchGeneralInfo;
+export default MatchScoreBoard;
--- client/src/components/Screens/Matches/MatchDetails/index.jsx
+++ client/src/components/Screens/Matches/MatchDetails/index.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-import MatchGeneralInfo from "./MatchGeneralInfo";
+import MatchScoreBoard from "./MatchScoreBoard";
 
 import "./MatchDetails.css";
 import { requestOneMatch } from "../../../axios";
 import { useParams } from "react-router-dom";
 
 const Match = () => {
-  console.log("sadasda");
   const { id } = useParams();
 
   const [matchData, setMatchData] = useState(null);
@@ -23,8 +22,8 @@ const Match = () => {
   }
 
   return (
-    <div className="match-page">
-      <MatchGeneralInfo  data={matchData} />
+    <div>
+      <MatchScoreBoard data={matchData} />
     </div>
   );
 };
