ddd7649ca6560a1c4f5066941e2d238482d0ae5d
ydelipinar <ydelipinar@sabanciuniv.edu>
2023-01-04 00:20:59 +0300

    New HomePage created
    Existing homepage which weas a only empty space with log out button changed to display more things.
    For example, right now it displays the top rated referees.
    Log out button also changed and new front-end design added to whole page.
    Still in development design and features will be changed as soon as possible

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "./components/Screens/HomePage/components/theme";
+import { useSelector } from "react-redux";
+
 import HomePage from "./components/Screens/HomePage";
 import Signup from "./components/Screens/SignUp";
 import Login from "./components/Screens/Signin";
@@ -14,7 +18,8 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import UserPage from "./components/Screens/UserPage/UserPage";
 import Ref from "./components/Screens/Referee";
-
+import { useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
 import EmailVerify from "./components/Screens/EmailVerify";
 import ForgotPassword from "./components/Screens/ForgotPassword";
 import PasswordReset from "./components/Screens/PasswordReset";
@@ -31,45 +36,52 @@ import Match from "./components/Screens/Matches/MatchDetails";
 
 function App() {
   const user = localStorage.getItem("token");
+  const theme = useMemo(() => createTheme(themeSettings('light')), ['light']);
   return (
+    
     <GoogleOAuthProvider clientId="330490937140-hmot7hf3u41oijddu2efks7j3ffvoig0.apps.googleusercontent.com">
-      <Navbar />
-      <Routes>
-        {user && <Route path="/" exact element={<HomePage />} />}
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
+       <Navbar />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+     
+          {user && <Route path="/" exact element={<HomePage />} />}
+        
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
 
-        <Route path="/profile" exact element={<UserPage />} />
-        <Route path="/profile/update" exact element={<UserProfile />} />
-        <Route path="/profile/delete" exact element={<DeleteProfile />} />
+          <Route path="/profile" exact element={<UserPage />} />
+          <Route path="/profile/update" exact element={<UserProfile />} />
+          <Route path="/profile/delete" exact element={<DeleteProfile />} />
 
-        <Route path="/referees" exact element={<Ref />} />
-        <Route path="/refereePanel" exact element={<RefereeAdminPanel />} />
-        <Route
-          path="/refereePanel/updateReferee/:id"
-          exact
-          element={<UpdateReferee />}
-        />
-        <Route
-          path="/refereePanel/deleteReferee/:id"
-          exact
-          element={<DeleteReferee />}
-        />
-        <Route
-          path="/refereePanel/addReferee"
-          exact
-          element={<AddReferee />}
-        />
-        <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
+          <Route path="/referees" exact element={<Ref />} />
+          <Route path="/refereePanel" exact element={<RefereeAdminPanel />} />
+          <Route
+            path="/refereePanel/updateReferee/:id"
+            exact
+            element={<UpdateReferee />}
+          />
+          <Route
+            path="/refereePanel/deleteReferee/:id"
+            exact
+            element={<DeleteReferee />}
+          />
+          <Route
+            path="/refereePanel/addReferee"
+            exact
+            element={<AddReferee />}
+          />
+          <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
 
-        <Route path="/referee/:id" exact element={<RefereeProfile />} />
-        <Route path="/standing" exact element={<Standings />} />
-        <Route path="/matches" exact element={<Matches />} />
-        <Route path="/matches/matchdetails/:id" element={<Match />} />
-      </Routes>
+          <Route path="/referee/:id" exact element={<RefereeProfile />} />
+          <Route path="/standing" exact element={<Standings />} />
+          <Route path="/matches" exact element={<Matches />} />
+          <Route path="/matches/matchdetails/:id" element={<Match />} />
+        </Routes>
+      </ThemeProvider>
     </GoogleOAuthProvider>
   );
 }
--- client/src/components/Screens/HomePage/index.jsx
+++ client/src/components/Screens/HomePage/index.jsx
@@ -1,8 +1,29 @@
 import styles from "./styles.module.css";
 import { googleLogout } from "@react-oauth/google";
-
+import { useEffect, useState } from "react";
 import React from "react";
+import { fetchReferee, fetchReview} from "../../axios";
+import FlexBetween from "./components/FlexBetween";
+import Header from "./components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import LogoutIcon from '@mui/icons-material/Logout';
+import {
+  Box,
+  IconButton,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid,} from "@mui/x-data-grid";
 
+//import { useGetDashboardQuery } from "state/api";
+import StatBox from "./components/StatBox";
 const HomePage = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -10,13 +31,193 @@ const HomePage = () => {
     googleLogout();
     window.location.reload();
   };
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const data = 3;
+ 
+  const [data1, setData] = useState([]);
+  useEffect(()=>{
+    fetchReferee().then(res=>{
+      setData(res)
+    })
+  },[])
+  console.log(data1);
 
+
+  const columns = [
+    {
+      field: "name",
+      headerName: "Name of the Referee",
+      flex: 1,
+    },
+    {
+      field: 'averagerating',
+      headerName: "Rating of the Referee",
+      flex: 1,
+      renderCell: (params) => `${Number(params.value).toFixed(2)}`,
+    },
+  ];
+  const handleOnCellClick = (params) => {
+    console.log("test");
+  };
+  const handleEvent: GridEventListener<'rowClick'> = (
+    params,  // GridRowParams
+    event,   // MuiEvent<React.MouseEvent<HTMLElement>>
+    details, // GridCallbackDetails
+  ) => {
+    console.log(params)
+  }
   return (
-    <div>
-      <button className={styles.logout_button} onClick={handleLogout}>
-        Logout
-      </button>
-    </div>
+
+      <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="Home Page" subtitle="Welcome to System" />
+
+        <Box>
+          <IconButton
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          size = 'large' onClick={handleLogout} >
+            <LogoutIcon fontSize="medium" />
+            LogOut
+          </IconButton>
+
+        </Box>
+      </FlexBetween>
+
+      <Box
+        mt="20px"
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="160px"
+        gap="20px"
+        sx={{
+          "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" },
+        }}
+      >
+        {/* ROW 1 */}
+        <StatBox
+          title="Total Customers"
+          value={data && data.totalCustomers}
+          increase="+14%"
+          description="Since last month"
+          icon={
+            <Email
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+        <StatBox
+          title="Sales Today"
+          value={1}
+          increase="+21%"
+          description="Since last month"
+          icon={
+            <PointOfSale
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem"
+          borderRadius="0.55rem"
+        >
+          
+        </Box>
+        <StatBox
+          title="Monthly Sales"
+          value={data}
+          increase="+5%"
+          description="Since last month"
+          icon={
+            <PersonAdd
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+        <StatBox
+          title="Yearly Sales"
+          value={data && data.yearlySalesTotal}
+          increase="+43%"
+          description="Since last month"
+          icon={
+            <Traffic
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
+          }
+        />
+
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 8"
+          gridRow="span 3"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              borderRadius: "5rem",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme.palette.background.alt,
+            },
+            "& .MuiDataGrid-footerContainer": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: "none",
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            loading={ !data1}
+            getRowId={(row) => row._id}
+            rows={(data1 && data1) || []}
+            columns={columns}
+            rowsPerPageOptions={[5, 10, 100]}
+            //onCellClick={handleOnCellClick(params: 	GridRowId)}
+            onRowClick = {handleEvent}
+            
+          />
+        </Box>
+        <Box
+          gridColumn="span 4"
+          gridRow="span 3"
+          backgroundColor={theme.palette.background.alt}
+          p="1.5rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            Sales By Category
+          </Typography>
+         
+          <Typography
+            p="0 0.6rem"
+            fontSize="0.8rem"
+            sx={{ color: theme.palette.secondary[200] }}
+          >
+            Breakdown of real states and information via category for revenue
+            made for this year and total sales.
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
